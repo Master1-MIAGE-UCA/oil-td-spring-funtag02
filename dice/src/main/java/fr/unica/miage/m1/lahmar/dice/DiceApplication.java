76dc0e0d6a8e8b3c6adb8c4ba87bd58fb0b37888
theo mac <theo.lahmar1@gmail.com>
2024-12-13 10:31:46 +0100

    spring project added

---
+++ dice/src/main/java/fr/unica/miage/m1/lahmar/dice/DiceApplication.java
@@ -0,0 +1,13 @@
+package fr.unica.miage.m1.lahmar.dice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceApplication.class, args);
+	}
+
+}
---
+++ dice/src/test/java/fr/unica/miage/m1/lahmar/dice/DiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.unica.miage.m1.lahmar.dice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
